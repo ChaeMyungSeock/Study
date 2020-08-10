9b1051eef4728b0e34ca0302c2dc657e70ce88b5
ChaeMyungSeock <64470601+ChaeMyungSeock@users.noreply.github.com>
2020-08-10 23:15:10 +0900

    Add files via upload

---
+++ Teamproj/Servo/servo_ex.ino.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int pos = 0;
+
+void setup()
+{
+  myservo.attach(9,600,2400);
+  
+}
+
+void loop()
+{
+  for(pos=0; pos<=180; pos+=1)
+  {
+    myservo.write(pos);
+    delay(15);
+  }
+  for(pos=180; pos>=0; pos-=1)
+  {
+    myservo.write(pos);
+    delay(15);
+  }
+}
