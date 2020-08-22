45c8b6bfce24b936ae011b7804a3f0b33388e257
ChaeMyungSeock <64470601+ChaeMyungSeock@users.noreply.github.com>
2020-08-22 21:38:38 +0900

    Update 땅따먹기.py

--- Algorithm/Programmers/땅따먹기.py
+++ Algorithm/Programmers/땅따먹기.py
@@ -5,7 +5,9 @@ def solution(land):
         land[i+1][1] += max(land[i][0],land[i][2],land[i][3])
         land[i+1][2] += max(land[i][1],land[i][0],land[i][3])
         land[i+1][3] += max(land[i][1],land[i][2],land[i][0])
-        
+# 각각의 다음 타일에서 그 전 타일의 최댓값을 각각 더해 나간다
+# 그러면 마지막 타일에서는 각각의 시작타일에서의 최댓값들이 나옴
+# 그 중에서 최대값을 뽑아내면 된다   
         answer = max(land[-1])
 
 
