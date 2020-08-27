3e05a4775888ee1f011593a5f26a2f30d5954af2
ChaeMyungSeock <64470601+ChaeMyungSeock@users.noreply.github.com>
2020-08-27 23:28:35 +0900

    Add files via upload

---
+++ Algorithm/Programmers/JadenCase 문자열만들기.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = ''
+    s = s.capitalize()
+    # 맨 첫 글자가 숫자이든 영문이든 일단 대문자(숫자는 포함 안됨)
+    for i in range(len(s)):
+        a=''
+        
+    return s
+answer = ""
+s = "3people unFollowed me"
+s = s.lower()
+s = s.split(" ")
+for i in s:
+    i = i.capitalize()
+    answer += i + " "
+print(answer)
