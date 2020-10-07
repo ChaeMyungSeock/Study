281f382f1183b7070e9877c78d0eccbba2f95dbc
ChaeMyungSeock <64470601+ChaeMyungSeock@users.noreply.github.com>
2020-10-07 15:21:13 +0900

    Update data_npy.py

--- efficientnet/efficientnet/data_npy.py
+++ efficientnet/efficientnet/data_npy.py
@@ -4,39 +4,50 @@ import tensorflow as tf
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import Element, dump, ElementTree
 import cv2 as cv
-# caltech_dir = '/data/mask/data/within_mask'
-# caltech_dir1 = '/data/mask/data/without_mask'
-# test_dir = '/data/mask/test/test'
-caltech_dir = '/home/john/Study/pytorch_retinaface/sg_maskdata/test/data/withmask'
-caltech_dir1 = '/home/john/Study/pytorch_retinaface/sg_maskdata/test/data/withoutmask'
 
 
-files = glob.glob(caltech_dir+"/*.*")
+# train_data path
+# caltech_dir1 = '/home/ubuntu/data/mask/train/within_mask'
+# caltech_dir2 = '/home/ubuntu/data/mask/train/without_mask'
+
+# test_data path
+caltech_dir1 = '/home/ubuntu/data/mask/test/within_mask'
+caltech_dir2 = '/home/ubuntu/data/mask/test/without_mask'
+
+
+# img glob
 files1 = glob.glob(caltech_dir1+"/*.*")
+files2 = glob.glob(caltech_dir2+"/*.*")
 
-# files = glob.glob(caltech_dir+"/*.*")
-# files1 = glob.glob(test_dir+"/*.*")
 
 x=[]
-for img in files:
+for img in files1:
     img = Image.open(img)
     img = img.convert("RGB")
     data = np.asarray(img)
     x.append(data)
-# print('1')
-for img1 in files1:
+print('1')
+
+for img1 in files2:
     img1 = Image.open(img1)
     img1 = img1.convert("RGB")
     data = np.asarray(img1)
     x.append(data)
 print('0')
 
+# numpy
 x = np.array(x)
-# x = x.astype(float) / 255
 print(x.shape)
 
-np.save('/home/john/Study/pytorch_retinaface/sg_maskdata/test/data/label/test.npy',x)
+# npy save
+np.save('/home/ubuntu/data/mask/label/mask_test_data.npy',x)
 
-# np.save('/home/john/Study/efficientnet/data/mask_test_0',x)
-# (282, 224, 224, 3)
 print('끝났습니다')
+
+
+# train
+# (4028, 224, 224, 3)
+
+# test
+# (198, 224, 224, 3)
+
